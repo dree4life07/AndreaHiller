49adb3d0e3315545728502eb64fac262dfa143c0
Andrea <Andrea@github.com>
2021-03-03 14:17:13 -0600

    whole lotta love

--- resources/app.js
+++ resources/app.js
@@ -1,9 +1,13 @@
-// Un-named function that runs itself immediately.
 (function() {
     var html = document.querySelector("html");
     var burger = document.querySelector(".burger");
     burger.addEventListener("click", function() {
-        // Toggle nav by toggling the nav-active CSS class on the document's <html> element.
         html.classList.toggle("nav-active");
+        /*if (("html").hasClass("nav-active")) {
+            ("html").removeClass("nav-active").addClass("not-active");
+        }
+        else {
+            ("html").addClass("nav-active");
+        }*/
     });
-})();// <-- This is where it runs itself.
+})();
