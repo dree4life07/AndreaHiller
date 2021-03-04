baabb21263b5d07a7599911953ae79993c75d3cc
Andrea <Andrea@github.com>
2021-03-04 14:29:58 -0600

    meh

--- resources/app.js
+++ resources/app.js
@@ -3,11 +3,18 @@
     var burger = document.querySelector(".burger");
     burger.addEventListener("click", function() {
         html.classList.toggle("nav-active");
-        /*if (("html").hasClass("nav-active")) {
+    }, function() {
+        html.classList.toggle("not-active");
+    });
+})();
+
+
+
+
+
+/*if (("html").hasClass("nav-active")) {
             ("html").removeClass("nav-active").addClass("not-active");
         }
         else {
             ("html").addClass("nav-active");
         }*/
-    });
-})();
