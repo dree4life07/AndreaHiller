6d780a3570e0785730420625672e0d22d5f148a8
Andrea <Andrea@github.com>
2021-03-04 16:45:56 -0600

    added jazz to links

--- resources/app.js
+++ resources/app.js
@@ -1,13 +1,24 @@
-(function() {
+let menuToggle = (function() {
     var html = document.querySelector("html");
     var burger = document.querySelector(".burger");
-    burger.addEventListener("click", function() {
+    var links = document.querySelector(".nav-links");
+    burger.addEventListener("click", () => {
         html.classList.toggle("nav-active");
-    }, function() {
-        html.classList.toggle("not-active");
+    }),
+    links.addEventListener("click", () => {
+        html.classList.toggle("nav-active");
     });
-})();
+})
+menuToggle();
 
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.   body.classList.remove("resize-animation-stopper");
+  }, 768);
+});
 
 
 
