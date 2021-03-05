05c0570e247e2591c425bd6438e567f8e63c565a
Andrea <Andrea@github.com>
2021-03-04 18:07:39 -0600

    added cus-nav

--- resources/app.js
+++ resources/app.js
@@ -16,8 +16,18 @@ window.addEventListener("resize", () => {
   document.body.classList.add("resize-animation-stopper");
   clearTimeout(resizeTimer);
   resizeTimer = setTimeout(() => {
-    document.   body.classList.remove("resize-animation-stopper");
+    document.body.classList.remove("resize-animation-stopper");
   }, 768);
+});
+
+window.addEventListener("scroll", () => {
+    let menuArea = document.getElementById('nav-shimmy');
+
+    if(window.pageYOffset > 20){
+        menuArea.classList.add("cus-nav");
+    }else{
+        menuArea.classList.remove("cus-nav")
+    }
 });
 
 
